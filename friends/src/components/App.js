a4dc951acebf316af818fcdbeef4beac66f4f826
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-15 14:54:00 -0400

    moved some files around, created a Login component with a basic render/return, and added a nav to the App component

---
+++ friends/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          {/* <PrivateRoute exact path="/protected" component={GasPrices} /> */}
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>Hey</div>
+        )
+    };
+};
+
+export default Login;
