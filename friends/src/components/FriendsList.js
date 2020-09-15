8fdefabf611195b20de55781e3f9e79be15f2c53
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-15 17:04:42 -0400

    finally got the form data to get onto the server

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -21,10 +21,18 @@ const StyledSection = styled.section`
     }
 `;
 
+const initialValue = {
+    name: '',
+    age: '',
+    email: '',
+    id: Date.now
+};
+
 class FriendsList extends React.Component {
     state = {
-        friends: []
-    }
+        friends: [],
+        form: initialValue
+    };
 
     componentDidMount() {
         this.getData();
@@ -36,28 +44,75 @@ class FriendsList extends React.Component {
           .then( response => {
             this.setState({
                 friends: response.data
-            })
+            })//
           })
           .catch( error => {
               console.log( error )
           });
     };
 
+    handleChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            form: {
+                ...this.state.form,
+                [ name ]: value
+            }
+        })
+    };
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        axiosWithAuth()
+            .post("/api/friends", this.state.form)
+            .then( response => {
+                console.log(response.data)
+                this.setState({
+                    friends: response.data
+                })
+                this.setState({form: initialValue})
+            })
+    };
+
     render() {
         return (
             <>
-                <form>
+                <form onSubmit={ this.handleSubmit }>
                     <div>
                         <label htmlFor='name'>Name: </label>
-                        <input id='name' />
+                        <input 
+                            id='name' 
+                            name='name'
+                            value={this.state.form.name}
+                            onChange={this.handleChange}
+                            type='text'
+                            placeholder='Enter name'
+                        />
                     </div>
                     <div>
                         <label htmlFor='email'>Email: </label>
-                        <input id='email' />
+                        <input 
+                            id='email' 
+                            name='email'
+                            value={this.state.form.email}
+                            onChange={this.handleChange}
+                            type='email'
+                            placeholder='Enter email'
+                        />
                     </div>
                     <div>
                         <label htmlFor='age'>Age: </label>
-                        <input id='age' />
+                        <input 
+                            id='age' 
+                            name='age'
+                            value={this.state.form.age}
+                            onChange={this.handleChange}
+                            type='number'
+                            placeholder='Enter age'
+                        />
                     </div>
                     <button type='submit'>add friend</button>
                 </form>
