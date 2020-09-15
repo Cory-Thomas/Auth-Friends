b613b2f9f8aaaeec26c6f808213bd0114e74ce91
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-15 15:39:23 -0400

    created a form and rearranged the form to be before/above the list of friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -24,17 +24,35 @@ class FriendsList extends React.Component {
 
     render() {
         return (
-            <section>
-                {this.state.friends.map( friend => {
-                    return (
-                        <div>
-                            <div>name: {friend.name}</div>
-                            <div>email: {friend.email}</div>
-                            <div>age: {friend.age}</div>
-                        </div>
-                    )
-                })}
-            </section>
+            <>
+                <form>
+                    <div>
+                        <label htmlFor='name'>Name: </label>
+                        <input id='name' />
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email: </label>
+                        <input id='email' />
+                    </div>
+                    <div>
+                        <label htmlFor='age'>Age: </label>
+                        <input id='age' />
+                    </div>
+                    <button type='submit'>add friend</button>
+                </form>
+                <section>
+                    <h2>Friends List</h2>
+                    {this.state.friends.map( friend => {
+                        return (
+                            <div>
+                                <div>name: {friend.name}</div>
+                                <div>email: {friend.email}</div>
+                                <div>age: {friend.age}</div>
+                            </div>
+                        )
+                    })}
+                </section>
+            </>
         )
     };
 };
